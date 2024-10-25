9e6ef93f665e5aecbb509ed1f03258ef2601d162
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-10-25 13:55:40 +0300

    exercise

---
+++ js_advanced/Advanced functions/commandProcessor.js
@@ -0,0 +1,36 @@
+function solution() {
+    let str = '';
+    return {
+        append: (s) => str += s,
+        removeStart: (n) => str = str.substring(n),
+        removeEnd: (n) => str = str.substring(0, str.length - n),
+        print: () => console.log(str)
+    }
+}
+
+// test case
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+
+firstZeroTest.append('again');
+
+firstZeroTest.removeStart(3);
+
+firstZeroTest.removeEnd(4);
+
+
+// test case
+
+let secondZeroTest = solution();
+
+secondZeroTest.append('123');
+
+secondZeroTest.append('45');
+
+secondZeroTest.removeStart(2);
+
+secondZeroTest.removeEnd(1);
+
+secondZeroTest.print();
