5ff73d711fdc0efefeae05d12958cb905e1ee003
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-11-02 20:23:50 +0200

    JavaScript quizz

---
+++ js_advanced/DOM/03.JavaScript-Quizz/solution.js
@@ -0,0 +1,37 @@
+function solve() {
+  let quizzie = document.getElementById('quizzie');
+  let sections = document.getElementsByTagName('section');
+
+  let result = document.querySelector('.results-inner h1');
+
+
+  let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents']
+  let userAnswers = 0;
+  let currentStep = 0;
+
+  quizzie.addEventListener('click', (e) => {
+    if (e.target.className === 'answer-text') {
+      sections[currentStep].style.display = 'none';
+
+      if (correctAnswers.some(answer => answer === e.target.innerHTML)) {
+        userAnswers++;
+      }
+
+      currentStep++;
+      if (currentStep < correctAnswers.length) {
+        sections[currentStep].style.display = 'block';
+      }
+
+      if (currentStep === 3) {
+  
+        document.querySelector('#results').style.display = 'block';
+        result.innerHTML = correctAnswers.length === userAnswers ?
+         'You are recognized as top JavaScript fan!' : `You have ${userAnswers} right answers`
+        
+      }
+
+    }
+  })
+
+
+}
