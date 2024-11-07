34e00068cf5df8b82279c59f15b61c2030806e2d
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-11-07 09:48:10 +0200

    dropdown menu with adding options

---
+++ js_advanced/DOM/18.Fill-Dropdown/dropdown.js
@@ -0,0 +1,16 @@
+function addItem() {
+
+    const elements = {
+        inputText : document.getElementById('newItemText'),
+        inputValue : document.getElementById('newItemValue'),
+        selectMenu : document.getElementById('menu')
+    };
+
+
+    const option = document.createElement('option');
+
+    option.textContent = elements.inputText.value;
+    option.value = elements.inputValue.value;
+
+    elements.selectMenu.appendChild(option);
+}
