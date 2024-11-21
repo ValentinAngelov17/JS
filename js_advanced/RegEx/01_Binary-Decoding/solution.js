1aeb7df1dfaf7e84fdd01c645068e38dfb1c60b8
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-11-21 15:45:35 +0200

    Implement binary string decoding function: extract, parse, and convert binary to text

---
+++ js_advanced/RegEx/01_Binary-Decoding/solution.js
@@ -0,0 +1,18 @@
+function solve() {
+    let input = document.getElementById('input').value;
+    let resultRef = document.getElementById('resultOutput');
+    
+    let num = input.split('').map(Number).reduce((acc, x) => acc + x, 0);
+    while (num > 9) {
+        num = num.toString().split('').map(Number).reduce((acc, x) => acc + x, 0);
+    }
+
+    let parsedNumber = input.slice(num, input.length - num);
+
+    let chars = [];
+    for (let i = 0; i < Math.ceil(parsedNumber.length / 8); i++) {
+        chars.push(String.fromCharCode(parseInt(parsedNumber.slice(i * 8, (i + 1) * 8), 2)));
+    }
+
+    resultRef.innerHTML = chars.filter(x => x.match(/[A-Za-z]/)).join('');
+}
