cb276557987cc84bf9f5e8dcf6fa864264dcd7b6
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-12-03 15:02:48 +0200

    worker

--- js_applications/objects_composition/array_extension.js
+++ js_applications/objects_composition/array_extension.js
@@ -11,18 +11,18 @@
         return this.slice(0, n);
     };
 
-    Array.prototype.sum = function() {
-        return this.reduce((acc, curr) => 
+    Array.prototype.sum = function () {
+        return this.reduce((acc, curr) =>
             acc + curr
         );
-   
+
     };
 
-    Array.prototype.average = function() {
+    Array.prototype.average = function () {
         return this.sum() / this.length;
     }
 
-    let arr = [1,2,3];
+    let arr = [1, 2, 3];
     console.log(arr.last());
     console.log(arr.skip(1));
     console.log(arr.take(1));
---
+++ js_applications/objects_composition/worker.js
@@ -0,0 +1,18 @@
+let someWorker = ((worker) => {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += (0.1 * worker.weight * worker.experience)
+        worker.dizziness = false;
+    }
+    return worker
+})
+    ({
+        weight: 120,
+
+        experience: 20,
+
+        levelOfHydrated: 200,
+
+        dizziness: true
+    })
+
+console.log(someWorker);
