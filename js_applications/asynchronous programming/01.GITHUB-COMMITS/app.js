43a817d7c896498dd7257ce8b7c96d338f3c0933
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-12-14 22:03:09 +0200

    exercise

---
+++ js_applications/asynchronous programming/01.GITHUB-COMMITS/app.js
@@ -0,0 +1,34 @@
+function loadCommits() {
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let commitsUl = document.getElementById('commits');
+
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+        .then((response) => {
+            if (response.status >= 400) {
+                throw ({
+                    responseStatus: response.status,
+                    responseStatusText: response.statusText
+                })
+            } else {
+                return response.json();
+
+            }
+        })
+        .then((response) => {
+            response.forEach(element => {
+                console.log(element.commit.author.name);
+                let li = document.createElement('li');
+                li.innerHTML = `${element.commit.author.name}: ${element.commit.message}`
+                commitsUl.appendChild(li);
+            });
+        })
+        .catch((response) => {
+            let error = document.createElement('li')
+            error.innerText = `${response[responseStatus]} - ${response[responseStatusText]}`
+            commitsUl.appendChild(error);
+            return error;
+        })
+
+
+}
